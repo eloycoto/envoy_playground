8c753143f4963b346f2c62923966ed5d2d2ec767
Eloy Coto <eloy.coto@gmail.com>
2020-04-12 18:08:35 +0200

    WASM: Rust proxy-wasm first example
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

---
+++ wasm_rust/filter/src/lib.rs
@@ -0,0 +1,30 @@
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use chrono::{DateTime, Utc};
+use log::info;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use std::time::Duration;
+
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(HelloWorld) });
+}
+
+struct HelloWorld;
+
+impl Context for HelloWorld {}
+
+impl RootContext for HelloWorld {
+    fn on_vm_start(&mut self, _: usize) -> bool {
+        info!("Hello, World!");
+        self.set_tick_period(Duration::from_secs(5));
+        true
+    }
+
+    fn on_tick(&mut self) {
+        let datetime: DateTime<Utc> = self.get_current_time().into();
+        info!("It's {}", datetime);
+    }
+}
